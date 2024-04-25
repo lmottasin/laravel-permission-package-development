149ca39d4eb030bfde16dd5cec7fcf96d4431138
Md. Mottasin Lemon <lmottasin@gmail.com>
2024-04-25 10:36:35 +0600

    feat: autoloading the package src folder and defining namespace, and register package service provider in extra keyword

---
+++ packages/lmottasin/laravel-permission-editor/src/Providers/PermissionEditorServiceProvider.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Lmottasin\LaravelPermissionEditor\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class PermissionEditorServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        //
+    }
+}
